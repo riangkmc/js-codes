d28ad3973c7cb6b39e55410f9fcaf7e458f2a49e
Rian <riaann.barbosa@gmail.com>
2025-04-19 04:29:48 -0300

    removendo arquivos indesejados

--- media1.js
+++
@@ -1,6 +0,0 @@
-let n1 = prompt("digite um numero")
-n1 = parseFloat(n1)
-let n2 = prompt("digite outro numero")
-n2 = parseFloat(n2)
-media = (n1 + n2) / 2
-alert( media > 7);
--- prompt.js
+++
@@ -1,6 +0,0 @@
-let n1 = prompt("digite um numero")
-n1 = parseInt(n1)
-let n2 = prompt("digite outro numero")
-n2 = parseInt(n2)
-resultado = n1 + n2
-alert(resultado)
